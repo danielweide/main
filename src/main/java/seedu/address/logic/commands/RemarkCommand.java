6fe94ca9ac9b92a1bb091df40a2872ab7096d20e
ZANARKAND\Daniel <danielkwang@outlook.com>
2017-10-10 16:37:44 +0800

    v1.1 test e

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -9,21 +9,20 @@ import seedu.address.logic.commands.exceptions.CommandException;
  */
 public class RemarkCommand extends UndoableCommand {
 
-public static final String COMMAND_WORD = "remark";
+    public static final String COMMAND_WORD = "remark";
 
-public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the remark of the person identified "
-        + "by the index number used in the last person listing. "
-        + "Existing remark will be overwritten by the input. \n"
-        + "Parameters: INDEX (must be a positive integer) "
-        + PREFIX_REMARK + "[REMARK]\n"
-        + "Example: " + COMMAND_WORD + " 1 "
-        + PREFIX_REMARK + "Likes to swim.";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the remark of the person identified "
+            + "by the index number used in the last person listing. "
+            + "Existing remark will be overwritten by the input. \n"
+            + "Parameters: INDEX (must be a positive integer) "
+            + PREFIX_REMARK + "[REMARK]\n"
+            + "Example: " + COMMAND_WORD + " 1 "
+            + PREFIX_REMARK + "Likes to swim.";
 
-public static final String MESSAGE_NOT_IMPLEMENTED_YET = "Remark command not implemented yet";
+    public static final String MESSAGE_NOT_IMPLEMENTED_YET = "Remark command not implemented yet";
 
-@Override
+    @Override
     protected CommandResult executeUndoableCommand() throws CommandException {
-    throw new CommandException(MESSAGE_NOT_IMPLEMENTED_YET);
+        throw new CommandException(MESSAGE_NOT_IMPLEMENTED_YET);
+    }
 }
-}
-
